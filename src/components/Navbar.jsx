6e9661ee09d8a76d5c8e555390cb80c4c9f32cf7
adityaji24 <adityatanwani059@gmail.com>
2026-06-14 16:23:20 +0530

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () =>
+      window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav
+      className={`navbar-custom ${
+        scrolled ? "navbar-scrolled" : ""
+      }`}
+    >
+      <div className="logo">
+        NayePankh Foundation
+      </div>
+
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+
+        <li>
+          <Link to="/volunteer">Volunteer</Link>
+        </li>
+
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
